26c2a49310593e469d7d065a13ee6671055b158a
hasnain-dev722 <kharalhasnain636@gmail.com>
2026-07-08 15:27:02 +0500

    learning

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const sidebarlinks = document.querySelectorAll(".list-group-item");
+const allpages = document.querySelectorAll(".content-box");
 sidebarlinks.forEach((link) => {
-  link.addEventListener("click", () => {
+  link.addEventListener("click", function(e) {
+    e.preventDefault();
     sidebarlinks.forEach((item) => {
       item.classList.remove("active");
     });
